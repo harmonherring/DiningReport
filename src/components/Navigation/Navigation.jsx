84312bf19f8b1019bf975720ccd4bf30143648e8
Harmon Herring <harmonherring@gmail.com>
2020-02-09 23:30:31 -0500

    Create temporary navbar

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from 'styled-components';
+import {
+    Navbar,
+    NavbarBrand,
+    NavbarToggler,
+    Collapse,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+} from 'reactstrap';
+
+const DiningNavbar = styled(Navbar)`
+    border-bottom: 2px solid #202d3b;
+    padding: 0;
+    height:60px;
+`
+
+class Navigation extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isOpen: false
+        };
+    }
+
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        return(
+            <DiningNavbar className="diningreport-nav" color="primary" dark expand="md">
+                <Container>
+                <NavbarBrand href="/">Dining<strong>Report</strong></NavbarBrand>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink href="/about">About</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="https://github.com/harmonherring/DiningReport">Github</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+                </Container>
+            </DiningNavbar>
+        );
+    }
+}
+
+export default Navigation;
